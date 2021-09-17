3cca87a48779f81047ed4316221f58e674abfa3f
Deon 'PrimeTime' Saunders <adsaunde@protonmail.com>
2021-09-17 02:07:43 -0400

    Squashed commit of the following:
    commit de16abede8afbc3dd15c2b4b62bc30a601f5d6e5
    Author: Deon 'PrimeTime' Saunders <adsaunde@protonmail.com>
    Date:   Fri Sep 17 01:54:00 2021 -0400
    
        init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="xJ64dheGnIeusujfFNQwNYQnkXcHEKuU";analytics._cdn="cdn.segment.com";analytics.SNIPPET_VERSION="4.15.0";
+  analytics.load("xJ64dheGnIeusujfFNQwNYQnkXcHEKuU");
+  analytics.page();
+  }}();
+</script>
+<template>
+  <div id="app">
+    <link rel="stylesheet" href="https://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
+    <Nav />
+    <router-view/>
+    <Footer />
+  </div>
+</template>
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+</style>
+<script>
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+
+export default {
+  components: {
+    Nav,
+    Footer
+  }
+}
+</script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,86 @@
+<template>
+  <b-navbar :type="headerColor" id="nav">
+    <template slot="brand">
+      <b-navbar-item href="/">
+        <strong>HN</strong>
+      </b-navbar-item>
+      <b-navbar-item v-if="isBeta">
+
+        <i>Beta tester</i>
+      </b-navbar-item>
+      <b-navbar-item v-if="isDev">
+
+        <span @click="rolloutOverride">DEV</span>
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item href="/">
+        Hot
+      </b-navbar-item>
+      <b-navbar-item v-if="ask" href="/ask">
+        Ask
+      </b-navbar-item>
+      <b-navbar-item v-if="show" href="/show">
+        Show
+      </b-navbar-item>
+    </template>
+    <template slot="end">
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <a class="button is-black" v-if="loggedIn" @click="logout">
+            Log out
+          </a>
+          <a class="button is-black" v-else href="/login">
+            Log in
+          </a>
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+<style lang="stylus">
+#nav {
+  margin-bottom: 15px
+}
+</style>
+<script>
+import Rox from 'rox-browser'
+import { Flags } from '../utils/flag'
+import { mapState, mapActions } from 'vuex'
+import { betaAccess } from '../utils/users'
+
+export default {
+  track() {
+    analytics.identify(' {{user.id}} ', {
+        name: '{{user.username}}',
+        company: '{{user.company}}'
+      });
+    analytics.page('Home', {
+      experiment: experiment.name,
+      flag: reporting.name,
+      value: reporting.value
+    });
+  },
+
+  data () {
+    return {
+      isDev: process.env.NODE_ENV === 'development',
+      headerColor: Flags.headerColor.getValue(),
+      ask: Flags.ask.isEnabled(),
+      show: Flags.show.isEnabled(),
+      isBeta: betaAccess()
+    }
+  },
+  methods: {
+    rolloutOverride: () => {
+      Rox.showOverrides()
+    },
+    ...mapActions([
+      'logout'
+    ])
+  },
+  computed: mapState([
+    'loggedIn'
+  ])
+}
+</script>
