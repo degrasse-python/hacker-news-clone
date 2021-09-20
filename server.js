57fdf30b20118e66d561a54a380f66070148d109
Deon 'PrimeTime' Saunders <adsaunde@protonmail.com>
2021-09-20 14:46:05 -0400

    try again

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require('express')
-const serveStatic = require("serve-static")
 const history = require('connect-history-api-fallback')
 const path = require('path')
-
+const serveStatic = require("serve-static")
 
 //const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
 // start express server
 app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
-
 app.use(history({
   disableDotRule: true,
   verbose: true,
   logger: console.log.bind(console)
 }))
+app.use(serveStatic(path.join(__dirname, '/dist')))
+
 
 //app.use(staticFileMiddleware);
 
