ea75f6d2a11e89d38dfcc60731391d8b1ea4804d
Deon 'PrimeTime' Saunders <adsaunde@protonmail.com>
2021-09-20 14:33:13 -0400

    try again

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const serveStatic = require("serve-static");
-const history = require('connect-history-api-fallback');
-const path = require('path');
+const express = require('express')
+const serveStatic = require("serve-static")
+const history = require('connect-history-api-fallback')
+const path = require('path')
 
 
 //const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
 // start express server
-app = express();
+app = express()
 app.use(history({
   disableDotRule: true,
   verbose: true,
   logger: console.log.bind(console)
-}));
-app.use(serveStatic(path.join(__dirname, 'dist')));
+}))
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
 //app.use(staticFileMiddleware);
 
 
 app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
-});
+  res.render(path.join(__dirname + '/dist/index.html'))
+})
 
 // view engine setup
-app.set('views', path.join(__dirname, 'src/views'));
+app.set('views', path.join(__dirname, 'src/views'))
 // app.set('view engine', 'pug');
 
 // app.use(staticFileMiddleware);
