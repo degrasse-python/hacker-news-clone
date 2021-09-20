30fd048e47dcd21957961d860aa1e88e3622acc9
Deon 'PrimeTime' Saunders <adsaunde@protonmail.com>
2021-09-20 14:17:29 -0400

    testing

--- server.js
+++ server.js
@@ -2,22 +2,28 @@ const express = require('express');
 const serveStatic = require("serve-static");
 const history = require('connect-history-api-fallback');
 const path = require('path');
-const staticFileMiddleware = express.static(__dirname);
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
 // start express server
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-
-// view engine setup
-app.set('views', path.join(__dirname, 'src/views'));
-app.set('view engine', 'pug');
-
-// app.use(staticFileMiddleware);
+app.use(staticFileMiddleware);
+// app.use(serveStatic(path.join(__dirname, 'dist')));
 app.use(history({
   disableDotRule: true,
   verbose: true,
   logger: console.log.bind(console)
 }));
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});
+
+// TRY THIS NEXT view engine setup
+// app.set('views', path.join(__dirname, 'src/views'));
+// app.set('view engine', 'pug');
+
+// app.use(staticFileMiddleware);
+
 
 const port = process.env.PORT || 80;
 app.listen(port);
