d6886b5144656ee157ae4bb790101eb3c83f2d43
Deon 'PrimeTime' Saunders <adsaunde@protonmail.com>
2021-09-20 13:40:25 -0400

    test again

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'pug');
 
 // app.use(staticFileMiddleware);
-app.use(history());
+app.use(history({
+  disableDotRule: true,
+  verbose: true,
+  logger: console.log.bind(console)
+}));
 
 const port = process.env.PORT || 80;
 app.listen(port);
