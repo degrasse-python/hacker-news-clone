7b1481b8184176ec7fa1a49ec4f398c2a240f0d9
Deon 'PrimeTime' Saunders <adsaunde@protonmail.com>
2021-09-20 15:28:18 -0400

    testing

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const serveStatic = require("serve-static")
 
 // start express server
 app = express()
-app.use(serveStatic(path.join(__dirname, '/dist')))
 app.use(history({
   disableDotRule: true,
   verbose: true,
   logger: console.log.bind(console)
 }))
+app.use(serveStatic(path.join(__dirname, '/dist')))
 
 //app.use(staticFileMiddleware);
 
