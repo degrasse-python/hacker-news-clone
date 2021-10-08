369891405103bf66d1a8d8f851a00f306caa2bc7
Deon 'PrimeTime' Saunders <adsaunde@protonmail.com>
2021-10-08 11:13:47 -0400

    fixed import

--- src/utils/flag.js
+++ src/utils/flag.js
@@ -2,7 +2,7 @@ import Rox from 'rox-browser'
 import { betaAccess, isLoggedIn, getCompany } from './users'
 import mixpanel from 'mixpanel-browser';
 // or with require() syntax:
-const mixpanel = require('mixpanel-browser');
+// const mixpanel = require('mixpanel-browser');
 
 // Enabling the debug mode flag is useful during implementation,
 // but it's recommended you remove it for production
